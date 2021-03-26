0eed2bc9fa732ac08b55c8158762c30dee964ccd
alvise <alvisebruniera@gmail.com>
2021-03-26 12:10:01 +0100

    Added function declaration, function calling still in progress

--- README.md
+++ README.md
@@ -12,12 +12,16 @@ This little project started both because of necessity (I wanted a program for wr
     * The sign is optional
     * The denominator is optional (you can't leave a pending `/` without denominator)
   * `<variable_name>` identifies a variable
-  * `<exp1> <exp2> (+|-|*|/)` identifies an arithmetic binary operation
+  * `<exp1> <exp2> (+|-|*|/)` performs an arithmetic binary operation
     * Operations have fixed arity so parenthesis are not needed
+  * `<exp1> <exp2> ~` perform a positive subtraction
+    * If the result is lesser than `0`, it returns `0`
+    * It returns the result otherwise
   * `<exp1> <exp2> <exp3> ?` if-then construct
     * If `<exp3>` equals `0`, drops `<exp2>` evaluates and returns `<exp1>`
     * If `<exp3>` *not* equals `0`, drops `<exp1>` evaluates and returns `<exp2>`
 * Commands (commands will not be pushed in stack):
+  * `<exp1> <function_name>|<arity>` declares a function of `<arity>` as `<exp1>`
   * `=<variable_name>` evaluates the expression on top of the stack and assigns its value to a variable
   * `=` evaluates the expression on top of the stack and prints it
   * `#` evaluates the expression on top of the stack and prints it, *and* pushes the result back in the stack
@@ -29,13 +33,13 @@ This little project started both because of necessity (I wanted a program for wr
   * `%` drops the entire stack
   * `;` comments the rest of the line
 
-## Completenes
+## Completeness
 
 With an extensive use of variables you should be able to evaluate most of the simple arithmetic expression with a little work on the user's end. More basic operations will be added in the near future, to lighten the user's work.
 Powers, integer division, and remainders will be added for sure; while roots and logarithms will need more time (if they will be implemented) because they will cause a loss of precision (due to irrationality).
 Infinite precision (with rational numbers) is a key element of the program, so anything that causes a fallback to floating point numbers (even Multiple-precision floating point numbers) will be neglected for the time being.
 
-About scripting and Turing-Completenes. For now, the user's ability to script is limited to: concatenating some number and a script file, and execute the resulting stream.
+About scripting and Turing-Completeness. For now, the user's ability to script is limited to: concatenating some number and a script file, and execute the resulting stream.
 It's still more than what your usual 4-op calculator can do, but it's not enough; the language is not Turing complete, it can neither iterate (which requires scoping and local variables) nor recurse and function composition (which require to have user defined functions in the first place). Around the time I decided to add variables, I also decided that TC is a necessity so stay tuned for that.
 
 ## Future developement
@@ -50,6 +54,7 @@ Near future:
 * [ ] User defined functions
   * [x] `if-else`
   * [ ] Recursion
+* [ ] Proof of Turing-Completeness
 * [ ] A decent prompt (with history)
 * [ ] Input from multiple files
 * [ ] Output to file (silent mode)
--- src/calculator.rs
+++ src/calculator.rs
@@ -5,6 +5,8 @@ use std::collections::HashMap;
 //use std::convert::TryFrom;
 use std::fmt;
 use std::string::String;
+use Object::*;
+use Token::*;
 
 // Readable tokens from command line
 #[derive(Logos, Debug, PartialEq, Clone)]
@@ -14,6 +16,16 @@ enum Token {
 
     #[regex("=[a-zA-Z]([a-zA-Z0-9]|-[a-zA-Z0-9]|_[a-zA-Z0-9])*", |lex| String::from(lex.slice()))]
     AssignVariable(String),
+
+    #[regex("[a-zA-Z]([a-zA-Z0-9]|-[a-zA-Z0-9]|_[a-zA-Z0-9])*\\|[0-9]+", |lex| String::from(lex.slice()))]
+    AssignFunction(String),
+
+    #[regex("\\$[0-9]+", |lex| {
+        let mut parse = lex.slice().split('$');
+        parse.next();
+        parse.next().unwrap().parse()
+    })]
+    Argument(usize),
 
     #[regex("[\\-\\+]?[0-9]+(/[0-9]+)?", |lex| lex.slice().parse())]
     Number(Rational),
@@ -29,6 +41,9 @@ enum Token {
 
     #[regex("/")]
     Divide,
+
+    #[regex("~")]
+    PositiveMinus,
 
     /*#[regex("\\^")]
     Power,
@@ -72,20 +87,21 @@ enum Token {
 #[derive(PartialEq, Clone)]
 enum Object {
     Variable(Rational),
-    Function(u64, Vec<Token>),
+    Function(usize, Vec<Token>),
 }
 
 // Implement Display for printing
 impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Token::Number(num) => write!(f, "{}", num),
-            Token::Plus => write!(f, "+"),
-            Token::Minus => write!(f, "-"),
-            Token::Times => write!(f, "*"),
-            Token::Divide => write!(f, "/"),
-            Token::If => write!(f, "?"),
-            Token::Identifier(name) => write!(f, "{}", name),
+            Number(num) => write!(f, "{}", num),
+            Plus => write!(f, "+"),
+            Minus => write!(f, "-"),
+            Times => write!(f, "*"),
+            Divide => write!(f, "/"),
+            If => write!(f, "?"),
+            PositiveMinus => write!(f, "~"),
+            Identifier(name) => write!(f, "{}", name),
             _ => write!(f, "Unprintable"),
         }
     }
@@ -121,10 +137,10 @@ impl Calculator {
     fn analyze(&mut self, token: Token) {
         match token {
             // Drop all errors
-            Token::Error => eprintln!("Dropped unrecognized token!"),
+            Error => eprintln!("Dropped unrecognized token!"),
 
             // Compute and print top of the stack
-            Token::Return => {
+            Return => {
                 if let Some(num) = self.compute() {
                     println!("> {}", num);
                 } else {
@@ -135,10 +151,10 @@ impl Calculator {
 
             // Compute and print top of the stack
             // Put result back in stack
-            Token::Partial => {
+            Partial => {
                 if let Some(num) = self.compute() {
                     println!("< {}", num);
-                    self.stack.push(Token::Number(num));
+                    self.stack.push(Number(num));
                 } else {
                     // Print error if arguments are missing
                     eprintln!("Incomplete expression, dropped stack");
@@ -146,24 +162,24 @@ impl Calculator {
             }
 
             // Compute top of stack and duplicate it
-            Token::Duplicate => {
+            Duplicate => {
                 if let Some(num) = self.compute() {
-                    self.stack.push(Token::Number(num.clone()));
-                    self.stack.push(Token::Number(num));
+                    self.stack.push(Number(num.clone()));
+                    self.stack.push(Number(num));
                 } else {
                     eprintln!("Incomplete expression, dropped stack");
                 }
             }
 
             // Compute and print entire stack
-            Token::Flush => {
+            Flush => {
                 while self.stack.len() > 0 {
-                    self.analyze(Token::Return);
+                    self.analyze(Return);
                 }
             }
 
             // Print all elements in stack without computing
-            Token::Print => {
+            Print => {
                 for token in &self.stack {
                     print!("{} ", token);
                 }
@@ -171,41 +187,86 @@ impl Calculator {
             }
 
             // Flush all stack without computing it
-            Token::Empty => {
+            Empty => {
                 self.stack.clear();
             }
 
             // Assign value to global variable
             // Drops previous value
-            Token::AssignVariable(mut name) => {
+            AssignVariable(mut name) => {
                 if let Some(val) = self.compute() {
                     // Remove '=' from the name before inserting it
                     name.remove(0);
-                    self.table.insert(name, Object::Variable(val));
+                    self.table.insert(name, Variable(val));
                 } else {
                     // Print error if arguments are missing
                     eprintln!("Incomplete expression, dropped stack");
                 }
             }
 
+            AssignFunction(name) => {
+                let mut to_drop = 1;
+                let mut i = self.stack.len();
+
+                // Split name from arity
+                let mut parse = name.split('|');
+                let function_name = String::from(parse.next().unwrap());
+                let arity = parse.next().unwrap().parse().unwrap();
+
+                while to_drop > 0 && i > 0 {
+                    match &self.stack[i - 1] {
+                        Identifier(name) => {
+                            // Check for self reference (for recursion)
+                            if name.eq(&function_name) {
+                                to_drop += arity - 1;
+                            } else {
+                                // Check table
+                                match self.table.get(name) {
+                                    Some(Function(arity, _)) => to_drop += arity - 1,
+                                    _ => to_drop -= 1,
+                                }
+                            }
+                        }
+
+                        Number(_) | Argument(_) => to_drop -= 1,
+
+                        Plus | Minus | Times | Divide | PositiveMinus => to_drop += 1,
+
+                        If => to_drop += 2,
+
+                        _ => panic!("Corrupted stack"),
+                    }
+
+                    // Moves index
+                    i -= 1;
+                }
+
+                if to_drop == 0 {
+                    self.table
+                        .insert(function_name, Function(arity, self.stack.split_off(i)));
+                } else {
+                    eprintln!("Incomplete function declaration, preserved stack");
+                }
+            }
+
             // Eliminate top of stack without computing it
-            Token::Drop => {
+            Drop => {
                 let mut to_drop = 1;
                 while to_drop > 0 {
                     match self.stack.pop() {
                         None => to_drop = 0,
 
-                        Some(Token::Identifier(name)) => match self.table.get(&name) {
-                            Some(Object::Function(arity, _)) => to_drop -= arity - 1,
+                        Some(Identifier(name)) => match self.table.get(&name) {
+                            Some(Function(arity, _)) => to_drop += arity - 1,
                             _ => to_drop -= 1,
                         },
 
-                        Some(Token::Number(_)) => to_drop -= 1,
+                        Some(Number(_)) | Some(Argument(_)) => to_drop -= 1,
 
-                        Some(Token::Plus) | Some(Token::Minus) | Some(Token::Times)
-                        | Some(Token::Divide) => to_drop += 1,
+                        Some(Plus) | Some(Minus) | Some(Times) | Some(Divide)
+                        | Some(PositiveMinus) => to_drop += 1,
 
-                        Some(Token::If) => to_drop += 2,
+                        Some(If) => to_drop += 2,
 
                         _ => panic!("Corrupted stack"),
                     }
@@ -223,28 +284,28 @@ impl Calculator {
         if let Some(token) = self.stack.pop() {
             match token {
                 // Return numers as is
-                Token::Number(num) => Some(num),
+                Number(num) => Some(num),
 
                 // Return variable's value
-                Token::Identifier(name) => match self.table.get(&name) {
-                    Some(Object::Variable(value)) => Some(value.clone()),
-                    Some(Object::Function(arity, ops)) => None,
+                Identifier(name) => match self.table.get(&name) {
+                    Some(Variable(value)) => Some(value.clone()),
+                    Some(Function(arity, ops)) => None,
                     None => {
                         eprintln!("Undefined name: {}", name);
                         None
                     }
                 },
 
-                Token::If => {
+                If => {
                     let test = self.compute();
 
                     if let Some(test) = test {
                         if test.cmp0() == std::cmp::Ordering::Equal {
-                            self.analyze(Token::Drop);
+                            self.analyze(Drop);
                             self.compute()
                         } else {
                             let res = self.compute();
-                            self.analyze(Token::Drop);
+                            self.analyze(Drop);
                             res
                         }
                     } else {
@@ -253,15 +314,13 @@ impl Calculator {
                 }
 
                 // This tokens will never be on stack
-                Token::Flush
-                | Token::Drop
-                | Token::Empty
-                | Token::AssignVariable(_)
-                | Token::Duplicate
-                | Token::Print
-                | Token::Error
-                | Token::Partial
-                | Token::Return => panic!("Corrupted stack"),
+                Flush | Drop | Empty | AssignVariable(_) | AssignFunction(_) | Duplicate
+                | Print | Error | Partial | Return => panic!("Corrupted stack"),
+
+                Argument(_) => {
+                    eprintln!("Arguments cannot be used outside of functions");
+                    None
+                }
 
                 // Binary operators are the only tokens left
                 _ => {
@@ -274,11 +333,19 @@ impl Calculator {
                         match token {
                             // Operations on borrowed values may return incomplete results
                             // Using incomplete results might make long operations faster
-                            Token::Plus => Some(a + &b),
-                            Token::Minus => Some(a - &b),
-                            Token::Times => Some(a * &b),
-                            Token::Divide => Some(a / &b),
-                            /*Token::Power => {
+                            Plus => Some(a + &b),
+                            Minus => Some(a - &b),
+                            Times => Some(a * &b),
+                            Divide => Some(a / &b),
+                            PositiveMinus => {
+                                let c = a / &b;
+                                if c.cmp0() > std::cmp::Ordering::Greater {
+                                    Some(c)
+                                } else {
+                                    Some(Rational::from(0))
+                                }
+                            }
+                            /*Power => {
                                 if let Some(1) = b.denom().to_u32() {
                                     let numer = a.numer().pow(b.numer());
                                     let denom = a.denom().pow(b.numer());
